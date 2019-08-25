a60acf4deefcabf3ea5c62d44de4545ee0269a07
Neeraj11331 <53964105+Neeraj11331@users.noreply.github.com>
2019-08-25 18:40:50 +0530

    Add files via upload

---
+++ simpleinterst1.py
@@ -0,0 +1,8 @@
+def Simpleinterest():
+    p=float(input("Input value of P:"))
+    rate=float(input("Input the value of rate:"))
+    n=float(input("Input the value of N:"))
+    si=p*n*rate/100
+    print("Simple interest:",si)
+    print("p+si",(p +si))
+Simpleinterest()    
